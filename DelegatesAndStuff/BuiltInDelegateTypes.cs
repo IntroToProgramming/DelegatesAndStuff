465c0945194ad7d5147803793455b133daca6398
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-19 12:38:35 -0400

    before lunch

---
+++ DelegatesAndStuff/BuiltInDelegateTypes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DelegatesAndStuff
+{
+    public class BuiltInDelegateTypes
+    {
+        private readonly ITestOutputHelper output;
+
+        public BuiltInDelegateTypes(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void ActionTypes()
+        {
+            // are a built-in delegate type for methods that
+            // return void
+            // and take 0 - 16 arguments
+            Action doIt;
+
+            doIt = () => output.WriteLine("Did it!");
+
+            doIt();
+
+            Action<int, string> doItMultipleTimes;
+            doItMultipleTimes = (times, message) =>
+            {
+                for(var t=0; t< times; t++)
+                {
+                    output.WriteLine(message);
+                }
+            };
+
+            doItMultipleTimes(10, "I just did it and doing it again!");
+
+        }
+
+        [Fact]
+        public void FuncTypes()
+        {
+            // Funcs are a built-in delegate type for methods that
+            // return something
+            // and take up to 16(?) arguments
+
+            Func<string, int> getLengthOf = (s) => s.Length;
+            Assert.Equal(9, getLengthOf("Solo, Han"));
+
+            Func<int, int, int> mathOp;
+
+            mathOp = (a, b) => a + b;
+            Assert.Equal(4, mathOp(2, 2));
+
+            mathOp = (a, b) => a * b;
+            Assert.Equal(9, mathOp(3, 3));
+
+            var calculator = new Dictionary<char, Func<int, int, int>>()
+            {
+                {'+', (a,b) => a + b },
+                { '-', (a,b) => a - b },
+                { '*' , (a,b) => a * b},
+                { '/', (a,b)=> a /b }
+            };
+
+            var sum = calculator['+'](10, 2);
+            Assert.Equal(12, sum);
+
+            var product = calculator['*'](5, 5);
+            Assert.Equal(25, product);
+        }
+    }
+}
---
+++ DelegatesAndStuff/MakingTagsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DelegatesAndStuff
+{
+    public class MakingTagsTests
+    {
+
+       [Fact]
+       public void CanMakeTagsProcedureally()
+        {
+            Assert.Equal("<h1>Hello</h1>", TagMaker("h1", "Hello"));
+            Assert.Equal("<h1>Goodbye</h1>", TagMaker("h1", "Goodbye"));
+            Assert.Equal("<p>Stuff</p>", TagMaker("p", "Stuff"));
+
+            string TagMaker(string element, string content)
+            {
+                return $"<{element}>{content}</{element}>";
+            }
+
+        }
+        [Fact]
+        public void CanMakeTagsOopStyle()
+        {
+            var h1Maker = new TagMaker("h1");
+            var pMaker = new TagMaker("p");
+
+            Assert.Equal("<h1>Hello</h1>", h1Maker.Make("Hello"));
+            Assert.Equal("<h1>Goodbye</h1>", h1Maker.Make("Goodbye"));
+            Assert.Equal("<p>Stuff</p>", pMaker.Make("Stuff"));
+        }
+
+        [Fact]
+        public void CanMakeTagsFunctionalStyle()
+        {
+            Func<string, Func<string, string>> tagMaker;
+
+            tagMaker = (tag) => (content) => $"<{tag}>{content}</{tag}>";
+
+            var h1Maker = tagMaker("h1");
+            var pMaker = tagMaker("p");
+
+            Assert.Equal("<h1>Hello</h1>", h1Maker("Hello"));
+            Assert.Equal("<h1>Goodbye</h1>", h1Maker("Goodbye"));
+            Assert.Equal("<p>Stuff</p>", pMaker("Stuff"));
+        }
+
+       
+    }
+
+    public class TagMaker
+    {
+        private string element;
+
+        public TagMaker(string element)
+        {
+            this.element = element;
+        }
+
+        public string Make(string content)
+        {
+            return $"<{element}>{content}</{element}>";
+        }
+    }
+}
